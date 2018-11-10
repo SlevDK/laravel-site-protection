3bf74c2df152be25f56d16d54b73e9ec8a2274f1
Slevin <35367910+SlevDK@users.noreply.github.com>
2018-11-10 11:29:00 +0200

    Update site-protection.php

--- src/config/site-protection.php
+++ src/config/site-protection.php
@@ -13,6 +13,6 @@ return [
     */
 
     'passwords' => env('SITE_PROTECTION_PASSWORDS'),
-    'exclde_post' => env('SITE_PROTECTION_EXCLUDE_POST'),
+    'exclude_post' => env('SITE_PROTECTION_EXCLUDE_POST'),
 
 ];
